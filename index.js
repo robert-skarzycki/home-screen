566f53a9a3ed100de3c23c550a9094f645464166
Robert Skarżycki <3417357+robert-skarzycki@users.noreply.github.com>
2019-01-09 18:18:28 +0100

    Calling API from JS

--- index.js
+++ index.js
@@ -3,5 +3,10 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.static('public'));
+app.use('/api/weather', function (req, res) {
+    res.status(200).send({
+        temperature: -3.1
+    });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,6 @@
 $(function () {
-    $('div[current-temperature]').text('loaded');
+    var $container = $('div[current-temperature]');
+    $.getJSON('/api/weather').then(function (data) {
+        $container.text(data.temperature);
+    });
 });
