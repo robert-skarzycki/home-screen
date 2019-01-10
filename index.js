d68688b23d34d0f4a4c3185b4ad6f23b0be36956
Robert Skarżycki <3417357+robert-skarzycki@users.noreply.github.com>
2019-01-10 15:07:24 +0100

    Weather forecast switched to iframe

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ require('dotenv').config();
 const express = require('express');
 const weatherCalculator = require('./server/weatherCalculator');
 const pollutionMeasurer = require('./server/pollutionMeasurer');
-const weatherForecast = require('./server/weatherForecast');
-
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -24,11 +22,5 @@ app.use('/api/pollution', function (req, res, next) {
     });
 });
 
-app.use('/api/forecast-url', function (req, res, next) {
-    weatherForecast.getImageUrl().then(imageData => {
-        res.status(200).send({ imageData: imageData });
-        next();
-    });
-});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
