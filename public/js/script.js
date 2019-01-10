5a608ec1feaba065e91b42769de353d05f9c6c24
Robert Skarżycki <3417357+robert-skarzycki@users.noreply.github.com>
2019-01-10 09:27:01 +0100

    Showing pollution measure

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,19 @@
 $(function () {
-    var $container = $('div[current-temperature]');
-    $.getJSON('/api/weather').then(function (data) {
-        $container.text(data.temperature);
-    });
+    var getTemperature = function () {
+        var $container = $('div[current-temperature]');
+        $.getJSON('/api/weather').then(function (data) {
+            $container.html(data.temperature + '&#x2103;');
+        });
+    };
+
+    var getPollution = function () {
+        var $container = $('div[current-pollution]');
+        $.getJSON('/api/pollution').then(function (data) {
+            $container.css('background-color', data.color);
+            $container.html('<p>' + data.description + '</p><p><i>' + data.advice + '</i></p>');
+        });
+    };
+
+    getTemperature();
+    getPollution();
 });
